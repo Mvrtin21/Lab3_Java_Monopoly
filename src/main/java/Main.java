1e425893ea992fab1a9b5893d1b97175b8fb7fea
Mvrtin21 <martin69gamer@gmail.com>
2025-06-13 00:06:24 -0400

    comit inicial creacion

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int opt;
+        do {
+            System.out.println("=== CAPITALIA ===");
+            System.out.println("1. Nueva Partida");
+            System.out.println("2. Mostrar Estado");
+            System.out.println("3. Salir");
+            System.out.print("Opción: ");
+            opt = Integer.parseInt(sc.nextLine());
+            switch (opt) {
+                case 1:
+                    System.out.println("-> Iniciando nueva partida...");
+                    // Game game = new Game_MartinArayaGaete_217813697();
+                    break;
+                case 2:
+                    System.out.println("-> Estado del juego:");
+                    // System.out.println(game);
+                    break;
+                case 3:
+                    System.out.println("¡Gracias por jugar CAPITALIA!");
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        } while (opt != 3);
+    }
+}
